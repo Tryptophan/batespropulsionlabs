bec3ffc5a1e08f0c3ed1a756d04067554c400975
Jacob Greenway <jagreenway12@gmail.com>
2020-09-13 17:47:50 -0500

    fix deployment system captions

--- src/routes/VerticalIntegrator.tsx
+++ src/routes/VerticalIntegrator.tsx
@@ -11,9 +11,9 @@ import AccelerationLogsImg from "../assets/vertical-integrator/acc.png";
 
 import ParachuteImg from "../assets/vertical-integrator/parachute.jpg";
 import GoresImg from "../assets/vertical-integrator/gores.jpg";
-import PlungerV1Img from "../assets/vertical-integrator/plungerv1.png";
-import PlungerV2Img from "../assets/vertical-integrator/plungerv2.png";
-import DeployerImg from "../assets/vertical-integrator/deployer.png";
+import RecDepSysV1Img from "../assets/vertical-integrator/plungerv1.png";
+import RecDepSysV2Img from "../assets/vertical-integrator/plungerv2.png";
+import PlungerV2Img from "../assets/vertical-integrator/deployer.png";
 
 import Board1Img from "../assets/vertical-integrator/board1.jpg";
 import Board2Img from "../assets/vertical-integrator/board2.png";
@@ -76,9 +76,9 @@ const KNDX1 = () => {
         <p>{text.verticalIntegrator.recovery.gas}</p>
 
         <CaptionedImages>
-          <CaptionedImage img={PlungerV1Img} caption="Plunger V1" />
+          <CaptionedImage img={RecDepSysV1Img} caption="Recovery Deployment System V1" />
+          <CaptionedImage img={RecDepSysV2Img} caption="Recovery Deployment System V2" />
           <CaptionedImage img={PlungerV2Img} caption="Plunger V2" />
-          <CaptionedImage img={DeployerImg} caption="Deployment Capsule" />
         </CaptionedImages>
 
         <h3>Parachute Ejection Testing Compilation</h3>
